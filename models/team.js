be704fd84a630972b3a42cd97eff33b4be9504d0
Misael Guerrero <mguerrerowest@gmail.com>
2023-10-24 17:55:37 -0400

    Create the team model/schema

---
+++ models/team.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const TeamSchema = new Schema({
+    name: { type: String, required: true, maxLength: 100 },
+    league: { type: Schema.Types.ObjectId, ref: "League", required: true, },
+    city: { type: String, required: true, maxLength: 100 }
+});
+
+// Virtual for team's URL
+LeagueSchema.virtual("url").get(function () {
+    return `/catalog/team/${this._id}`;
+  });
+
+module.exports = mongoose.model("Team", TeamSchema);
