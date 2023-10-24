8be85a83af9d7818a93f9f6fe9857d15ff10078e
Misael Guerrero <mguerrerowest@gmail.com>
2023-10-24 17:13:18 -0400

    Create kit model/schema

---
+++ models/kit.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const KitSchema = new Schema({
+    team: { type: Schema.Types.ObjectId, ref: "Team", required: true, maxLength: 100 },
+    season: { type: String, required: true, maxLength: 100 },
+  });
+
+// Virtual for kit's URL
+KitSchema.virtual("url").get(function () {
+    return `/catalog/kit/${this._id}`;
+  });
+
+module.exports = mongoose.model("Kit", KitSchema);
