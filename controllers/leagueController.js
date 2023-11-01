b4ee46f91abeec6237d4d62f69a633139b47d678
Misael Guerrero <mguerrerowest@gmail.com>
2023-11-01 16:38:20 -0400

    Modify function to display a detail page of a specified league

--- controllers/leagueController.js
+++ controllers/leagueController.js
@@ -1,4 +1,5 @@
 const League = require("../models/league");
+const Kit = require("../models/kit");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all Leagues.
@@ -10,7 +11,25 @@ exports.league_list = asyncHandler(async (req, res, next) => {
 
 // Display detail page for a specific League.
 exports.league_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: League detail: ${req.params.id}`);
+  const [league, kits] = await Promise.all([
+    League.findById(req.params.id).exec(),
+    Kit.find({}, "team season").populate('team').exec()
+  ]);
+
+  if(league === null){
+    const err = new Error('League not found')
+    err.status = 404
+    return next(err)
+  }
+
+  // Filter the kits that belong to the specified league
+  const kitsInLeague = kits.filter((kit) => kit.team.league.toString() === req.params.id);
+  
+  res.render('league_detail', {
+    title: 'League Detail',
+    league: league,
+    league_kits: kitsInLeague,
+  });
 });
 
 // Display League create form on GET.
