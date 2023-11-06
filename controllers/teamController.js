7ec3ffa93c89fc49abe597ffe1b83679a7b41716
Misael Guerrero <mguerrerowest@gmail.com>
2023-11-06 11:09:13 -0500

    Modify 'team_create_get' to get all leagues to be used in team creation

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -1,4 +1,5 @@
 const Team = require("../models/team");
+const League = require("../models/league")
 const Kit = require('../models/kit')
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
@@ -35,13 +36,22 @@ exports.team_detail = asyncHandler(async (req, res, next) => {
 
 // Display Team create form on GET.
 exports.team_create_get = asyncHandler(async (req, res, next) => {
-  res.render("team_form", { title: "Create Team" })
+  //Get all leagues, they're necessary for creating new teams
+  const allLeagues = await League.find().exec()
+
+  res.render("team_form", { title: "Create Team", leagues: allLeagues })
 });
 
 // Handle Team create on POST.
-exports.team_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Team create POST");
-});
+exports.team_create_post = [
+  // body("name")
+  //   .trim()
+  //   .isLength({ min: 3 })
+  //   .escape()
+  //   .withMessage("Team name must be specified.")
+  //   .isAlphanumeric()
+  //   .withMessage("First name has non-alphanumeric characters."),
+]
 
 // Display Team delete form on GET.
 exports.team_delete_get = asyncHandler(async (req, res, next) => {
