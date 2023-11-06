ee20a0cced81fbb2dbdc8c4af80d21ec4d745d46
Misael Guerrero <mguerrerowest@gmail.com>
2023-11-06 10:48:20 -0500

    Handle League creation on POST

--- controllers/leagueController.js
+++ controllers/leagueController.js
@@ -39,9 +39,50 @@ exports.league_create_get = (req, res, next) => {
 };
 
 // Handle League create on POST.
-exports.league_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: League create POST");
-});
+exports.league_create_post = [
+  // Validate and sanitize the name/country field.
+  body("name", "League name must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("country", "The country must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a league object with escaped and trimmed data.
+    const league = new League({ name: req.body.name, country: req.body.country });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("league_form", {
+        title: "Create League",
+        league: league,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      // Check if League with same name already exists.
+      const leagueExists = await League.findOne({ name: req.body.name })
+        .collation({ locale: "en", strength: 2 })
+        .exec();
+      if (leagueExists) {
+        // League exists, redirect to its detail page.
+        res.redirect(leagueExists.url);
+      } else {
+        await league.save();
+        // New league saved. Redirect to league detail page.
+        res.redirect(league.url);
+      }
+    }
+  }),
+]
 
 // Display League delete form on GET.
 exports.league_delete_get = asyncHandler(async (req, res, next) => {
