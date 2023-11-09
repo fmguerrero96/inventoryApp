60f9c4cb8cf85d05b148dae2738a13de7c814235
Misael Guerrero <mguerrerowest@gmail.com>
2023-11-08 22:31:17 -0500

    Modify function to handle kit instance creation on POST

--- controllers/kitinstanceController.js
+++ controllers/kitinstanceController.js
@@ -50,9 +50,59 @@ exports.kitinstance_create_get = asyncHandler(async (req, res, next) => {
 });
 
 // Handle KitInstance create on POST.
-exports.kitinstance_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: KitInstance create POST");
-});
+exports.kitinstance_create_post = [
+  body("kit")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Please specify a kit"),
+  body('price')
+    .trim()
+    .isLength({ min: 1 })
+    .isDecimal()
+    .withMessage('Please provide a valid price between 0 and 1000.')
+    .escape(),
+  body("size")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Please specify a size"),
+  body('in_stock')
+    .isBoolean(),
+  
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a kitInstance object with escaped and trimmed data.
+    const kitInstance = new KitInstance({
+      kit: req.body.kit,
+      price: req.body.price,
+      size: req.body.size,
+      in_stock: req.body.in_stock,
+    })
+
+    if (!errors.isEmpty()) {
+      // There are errors.
+      // Render form again with sanitized values and error messages.
+      const allKits = await Kit.find({}, "team season").populate("team").exec()
+
+      res.render("kitinstance_form", {
+        title: "Create kit instance",
+        kit_list: allKits,
+        selected_kit: kitInstance.kit._id,
+        errors: errors.array(),
+        kitInstance: kitInstance,
+      });
+      return;
+    } else {
+      // Data from form is valid
+      await kitInstance.save();
+      res.redirect(kitInstance.url);
+    }
+  })
+]
 
 // Display KitInstance delete form on GET.
 exports.kitinstance_delete_get = asyncHandler(async (req, res, next) => {
